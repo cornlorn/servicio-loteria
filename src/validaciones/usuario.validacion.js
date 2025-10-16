9e3d5ea75fa34473c0424fd04fb9de5760fdf6e4
cornlorn <sosadavid4280@gmail.com>
2025-10-15 22:40:22 -0600

    Validaciones de registro de usuario

---
+++ src/validaciones/usuario.validacion.js
@@ -0,0 +1,24 @@
+import { body } from "express-validator";
+
+export const validarRegistroUsuario = [
+    body("nombre")
+        .notEmpty()
+        .withMessage("El nombre es obligatorio")
+        .isString()
+        .withMessage("El nombre debe ser una cadena de texto"),
+    body("apellido")
+        .notEmpty()
+        .withMessage("El apellido es obligatorio")
+        .isString()
+        .withMessage("El apellido debe ser una cadena de texto"),
+    body("correo")
+        .notEmpty()
+        .withMessage("El correo es obligatorio")
+        .isEmail()
+        .withMessage("El correo debe ser válido"),
+    body("contrasena")
+        .notEmpty()
+        .withMessage("La contraseña es obligatoria")
+        .isLength({ min: 8 })
+        .withMessage("La contraseña debe tener al menos 8 caracteres"),
+];
